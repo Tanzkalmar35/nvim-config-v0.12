11e6fc7b7ba22262fb9f5737d47ca58caf97858e
Fabian Holler <fabianholler2005@gmail.com>
2025-11-15 11:27:27 +0000

    Added jdtls config, vim-doge and nvim-ufo

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,23 +1,31 @@
 vim.pack.add({
     { src = "https://github.com/lewis6991/gitsigns.nvim" },
     { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/saghen/blink.cmp", version = vim.version.range("^1") },
+    { src = "https://github.com/saghen/blink.cmp",               version = vim.version.range("^1") },
     { src = "https://github.com/ellisonleao/gruvbox.nvim" },
     { src = "https://github.com/ibhagwan/fzf-lua" },
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/m4xshen/autoclose.nvim" },
     { src = "https://github.com/folke/flash.nvim" },
-	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/mfussenegger/nvim-jdtls" },
+    { src = "https://github.com/kkoomen/vim-doge" },
+    { src = "https://github.com/kevinhwang91/nvim-ufo" },
+    { src = "https://github.com/kevinhwang91/promise-async" },
 })
 
 require('gitsigns').setup({ signcolumn = false })
-require('mason').setup({})
+require('mason').setup({
+    ensure_installed = {
+        "jdlts",
+    }
+})
 require('autoclose').setup({})
 require('flash').setup()
 require('nvim-treesitter.configs').setup({
-    ensure_installed = {"lua", "go", "rust", "java"},
-    highlight = {enable = true}
+    ensure_installed = { "lua", "go", "rust", "java" },
+    highlight = { enable = true }
 })
 
 require('blink.cmp').setup({
@@ -27,7 +35,7 @@ require('blink.cmp').setup({
         preset = "default",
         ["<C-space>"] = {},
         ["<C-p>"] = {},
-        ["<Tab>"] = {},
+        ["<Tab>"] = { "select_and_accept", "fallback" },
         ["<S-Tab>"] = {},
         ["<C-y>"] = { "show", "show_documentation", "hide_documentation" },
         ["<C-n>"] = { "select_and_accept" },
@@ -88,3 +96,75 @@ require('fzf-lua').setup({
         }
     }
 })
+
+local java_aug = vim.api.nvim_create_augroup("JavaJdtls", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = java_aug,
+    pattern = "java",
+    callback = function()
+        -- --- MANUALLY DEFINE THE PATH ---
+        -- Get the path to the jdtls executable installed by Mason
+        local mason_bin_path = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
+        -- --- VERIFY THE PATH (for safety) ---
+        -- This checks if the file actually exists before we try to use it
+        if vim.fn.executable(mason_bin_path) == 0 then
+            print("Error: jdtls executable not found at: " .. mason_bin_path)
+            print("Make sure Mason has installed jdtls.")
+            return
+        end
+
+        local jdtls = require("jdtls")
+        jdtls.start_or_attach({
+            -- We are now explicitly telling nvim-jdtls what command to run
+            cmd = { mason_bin_path },
+            root_dir = jdtls.setup.find_root({
+                "pom.xml",
+                "build.gradle",
+                "settings.gradle",
+                ".git",
+            }),
+            on_attach = function(client, bufnr)
+                local map = function(mode, lhs, rhs, desc)
+                    vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+                end
+
+                -- Standard LSP maps
+                map("n", "gd", vim.lsp.buf.definition, "Go to Definition")
+                map("n", "gr", vim.lsp.buf.references, "Go to References")
+                map("n", "gi", vim.lsp.buf.implementation, "Go to Implementation")
+                map("n", "K", vim.lsp.buf.hover, "Hover Documentation")
+                map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+
+                -- Java-specific refactoring maps
+                map("n", "<leader>jo", jdtls.organize_imports, "Organize Imports")
+                map("n", "<leader>jev", jdtls.extract_variable, "Extract Variable")
+                map("v", "<leader>jem", "<ESC><CMD>lua require('jdtls').extract_method(true)<CR>", "Extract Method")
+            end,
+        })
+    end,
+})
+
+-- ### nvim-ufo Konfiguration ###
+
+-- Globale Faltungseinstellungen
+vim.o.foldcolumn = "1" -- Zeigt eine Spalte für Faltungen an
+vim.o.foldlevel = 99   -- Standardmäßig alle Faltungen offen
+vim.o.foldlevelstart = 99 -- Beim Öffnen einer Datei alle Faltungen offen
+vim.o.foldenable = true  -- Faltungen aktivieren
+
+-- Wichtige Tastenbelegungen für UFO
+-- 'zR' öffnet alle Faltungen, 'zM' schließt alle Faltungen
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+
+-- UFO-Setup
+require("ufo").setup({
+    -- Sag UFO, es soll Treesitter für Faltungen verwenden.
+    -- Wenn Treesitter nicht verfügbar ist, soll es Einrückungen (indent) als Fallback nutzen.
+    provider_selector = function(bufnr, filetype, buftype)
+        return { "treesitter", "indent" }
+    end
+})
+
+-- ### Ende nvim-ufo ###
